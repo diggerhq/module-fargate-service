6970ca3ffbddf104060cd756ffa371e7cf9dffdf
motatoes <moe.habib9@gmail.com>
2021-10-11 20:04:54 +0300

    ignore alb listener

--- lb-http.tf
+++ lb-http.tf
@@ -10,6 +10,10 @@ resource "aws_alb_listener" "http" {
     target_group_arn = aws_alb_target_group.main.id
     type             = "forward"
   }
+
+  lifecycle {
+    ignore_changes = [port, protocol, default_action]
+  }
 }
 
 resource "aws_lb_listener" "https" {
@@ -23,6 +27,11 @@ resource "aws_lb_listener" "https" {
     target_group_arn = aws_alb_target_group.main.id
     type             = "forward"
   }
+
+  lifecycle {
+    ignore_changes = [port, protocol, ssl_policy, certificate_arn, default_action]
+  }
+
 }
 
 resource "aws_lb_listener_certificate" "dggr_domain" {
