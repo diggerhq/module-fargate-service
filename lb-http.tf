89a2546befd3ca7a226d649b50095cc9821725e5
alexey <alexey@digger.dev>
2022-03-11 10:44:46 +0000

    copy changes from fargate-target

--- lb-http.tf
+++ lb-http.tf
@@ -17,27 +17,22 @@ resource "aws_alb_listener" "http" {
 }
 
 resource "aws_lb_listener" "https" {
-  count = var.lb_ssl_certificate_arn==null ? 0 : 1
+  count = (var.lb_ssl_certificate_arn==null && var.dggr_acm_certificate_arn==null) ? 0 : 1
   load_balancer_arn = aws_alb.main.arn
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = var.lb_ssl_certificate_arn
+  certificate_arn   = var.lb_ssl_certificate_arn==null ? var.dggr_acm_certificate_arn : var.lb_ssl_certificate_arn
   default_action {
     target_group_arn = aws_alb_target_group.main.id
     type             = "forward"
   }
-
-  lifecycle {
-    ignore_changes = [port, protocol, ssl_policy, default_action]
-  }
-
 }
 
-resource "aws_lb_listener_certificate" "dggr_domain" {
-  count = var.dggr_acm_certificate_arn==null ? 0 : 1
-  listener_arn = aws_lb_listener.https[0].arn
-  certificate_arn   = var.dggr_acm_certificate_arn
+resource "aws_lb_listener_certificate" "lb_listener_cert" {
+   count = (var.lb_ssl_certificate_arn==null && var.dggr_acm_certificate_arn==null) ? 0 : 1
+   listener_arn = aws_lb_listener.https[0].arn
+   certificate_arn   = var.lb_ssl_certificate_arn==null ? var.dggr_acm_certificate_arn : var.lb_ssl_certificate_arn
 }
 
 resource "aws_security_group_rule" "ingress_lb_http" {
